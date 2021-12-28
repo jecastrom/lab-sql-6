4eae2cb27d3ad3971e767bf0456608582ce8c774
jecastrom <jcastro.de@outlook.com>
2021-12-28 02:22:21 +0100

    Solutions

--- files_for_lab/SQL Lab 6 script only.sql
+++ files_for_lab/SQL Lab 6 script only.sql
@@ -1,1 +1,12 @@
-a
+
+-- set @file_path = "C:\Users\jcast\lab-sql-6\files_for_lab\films_2020.csv";
+-- set @file_path = replace(@file_path, '\\', '\\\\');
+-- select @file_path;
+
+load data local infile "C:\\Users\\jcast\\lab-sql-6\\files_for_lab\\films_2020.csv"
+into table film
+fields terminated by ','
+lines terminated by '\n'
+ignore 1 lines
+(film_id,title,`description`,release_year,language_id,original_language_id,rental_duration,rental_rate,length,replacement_cost,rating)
+
